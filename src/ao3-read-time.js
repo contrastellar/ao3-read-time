2eed44b61b8e54bdff5333bbc7ea5df845152ada
Gabriella Stimac <gstimac@msudenver.edu>
2024-04-30 20:21:46 -0600

    Edited `ao3-read-time.js` -> Moved extension body into a function definition, adjusted how the readout displays on AO3, commented most of the main() function

--- src/ao3-read-time.js
+++ src/ao3-read-time.js
@@ -1,22 +1,7 @@
-var wpm = 500;
-const seconds = 60;
-const milliseconds = 1000;
-
-var wordCountQuerySelector = document.querySelectorAll('dd.words');
-
-for(let wordCountElement of wordCountQuerySelector){
-    let wordCount = parseInt(removeCommas(wordCountElement.innerText));
-    let minutesToRead = calculateMinutesToRead(wordCount, wpm);
-
-    let roundedMinutesToRead = Math.round(minutesToRead);
-
-    const dt = document.createElement('dt');
-    const node = document.createTextNode(" Time to read in minutes: " + roundedMinutesToRead.toString());
-
-    dt.appendChild(node);
-
-    wordCountElement.parentElement.appendChild(dt);
-};
+/**
+ * Simple extension, which adds a new read time element to AO3 listings.
+ * @author "Gabriella 'Contrastellar' Agathon"
+ */
 
 /**
  * Removes commas from a word count and returns the integer value
@@ -36,4 +21,35 @@ function removeCommas(wordCountInnerText){
  */
 function calculateMinutesToRead(wordCount, wpm) {
     return wordCount / wpm;
-  }
+}
+
+function main() {
+    var wpm = 500; // words per minute -- this needs to be able to be set by the user
+
+    var wordCountQuerySelector = document.querySelectorAll('dd.words');
+
+    for(let wordCountElement of wordCountQuerySelector){
+        // convert the word count from a string to an integer, removing the comma(s) from it
+        let wordCount = parseInt(removeCommas(wordCountElement.innerText));
+        // calculate the number of minutes it would take to read the given number of words at the given words per minute speed
+        let minutesToRead = calculateMinutesToRead(wordCount, wpm);
+
+        // round the number of minutes to the nearest integer
+        let roundedMinutesToRead = Math.round(minutesToRead);
+
+        // create new dt element to hold the time to read in minutes
+        const dt = document.createElement('dt');
+        // create new text node to hold the time to read in minutes
+        const node = document.createTextNode("Reading Time: " + roundedMinutesToRead.toString() + " minutes");
+
+        // append the new text node to the new dt element
+        dt.appendChild(node);
+
+        // append the new dt element to the parent element of the word count
+        // this is a dl element
+        wordCountElement.parentElement.appendChild(dt);
+    };
+
+}
+
+main();
